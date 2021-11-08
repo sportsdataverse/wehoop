1e20383c8a0abd38f1a5682c2fa0291635b76166
saiemgilani <saiem.gilani@gmail.com>
2021-11-08 18:20:05 -0500

    oops on this example not being dont test

--- R/espn_wnba_data.R
+++ R/espn_wnba_data.R
@@ -467,11 +467,10 @@ espn_wnba_teams <- function(){
 #' @import rvest
 #' @export
 #' @examples
-#' # Get schedule from 2020 season (returns 1000 results, max allowable.)
-#' # Must iterate through dates to get full year's schedule, as below:
-#' espn_wnba_scoreboard (season = 2020)
 #' # Get schedule from date 2020-08-29
-#' espn_wnba_scoreboard (season = "20200829")
+#' \donttest{
+#'   espn_wnba_scoreboard (season = "20200829")
+#' }
 
 espn_wnba_scoreboard <- function(season){
   
