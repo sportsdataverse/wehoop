823260cde47307d983afdb1d1352bd2c858209df
saiemgilani <saiem.gilani@gmail.com>
2022-05-24 09:15:02 -0400

    add identifying columns to pbp for wbb and wnba
    add headshot url to player_box functions

--- tests/testthat/test-espn_wbb_player_box.R
+++ tests/testthat/test-espn_wbb_player_box.R
@@ -1,16 +1,41 @@
 
-cols <- c("athlete_display_name", "team_short_display_name", 
-          "min", "fg", "fg3", "ft", "oreb", "dreb", "reb", 
-          "ast", "stl", "blk", "to", "pf", "pts", 
-          "starter", "ejected", "did_not_play", "active", 
-          "athlete_jersey", "athlete_id", "athlete_short_name", 
-          "athlete_headshot_href", "athlete_position_name", 
-          "athlete_position_abbreviation", "team_name", "team_logo", 
-          "team_id", "team_abbreviation", "team_color", "team_alternate_color")
-
 test_that("ESPN - WBB Player Box", {
   skip_on_cran()
   x <- espn_wbb_player_box(game_id = 401276115)
+  cols <- c(
+    "athlete_display_name",
+    "team_short_display_name",
+    "min",
+    "fg",
+    "fg3",
+    "ft",
+    "oreb",
+    "dreb",
+    "reb",
+    "ast",
+    "stl",
+    "blk",
+    "to",
+    "pf",
+    "pts",
+    "starter",
+    "ejected",
+    "did_not_play",
+    "active",
+    "athlete_jersey",
+    "athlete_id",
+    "athlete_short_name",
+    "athlete_headshot_href",
+    "athlete_position_name",
+    "athlete_position_abbreviation",
+    "team_name",
+    "team_logo",
+    "team_id",
+    "team_abbreviation",
+    "team_color",
+    "team_alternate_color"
+  )
+  
   expect_equal(colnames(x), cols)
   expect_s3_class(x, "data.frame")
 })
--- tests/testthat/test-espn_wnba_player_box.R
+++ tests/testthat/test-espn_wnba_player_box.R
@@ -1,13 +1,41 @@
 
-cols <- c("athlete_display_name", "team_short_display_name", 
-          "min", "fg", "fg3", "ft", "oreb", "dreb", "reb", "ast", "stl", "blk", 
-          "to", "pf", "+/-", "pts", "starter", "ejected", "did_not_play", "active", 
-          "athlete_jersey", "athlete_id", "athlete_short_name", "athlete_position_name", 
-          "athlete_position_abbreviation", "team_name", "team_logo", 
-          "team_id", "team_abbreviation", "team_color", "team_alternate_color")
 
 test_that("ESPN - WNBA Player Box", {
   skip_on_cran()
+  cols <- c(
+    "athlete_display_name",
+    "team_short_display_name",
+    "min",
+    "fg",
+    "fg3",
+    "ft",
+    "oreb",
+    "dreb",
+    "reb",
+    "ast",
+    "stl",
+    "blk",
+    "to",
+    "pf",
+    "+/-",
+    "pts",
+    "starter",
+    "ejected",
+    "did_not_play",
+    "active",
+    "athlete_jersey",
+    "athlete_id",
+    "athlete_short_name",
+    "athlete_headshot_href",
+    "athlete_position_name",
+    "athlete_position_abbreviation",
+    "team_name",
+    "team_logo",
+    "team_id",
+    "team_abbreviation",
+    "team_color",
+    "team_alternate_color"
+  )
   x <- espn_wnba_player_box(game_id = 401244185)
   expect_equal(colnames(x), cols)
   expect_s3_class(x, "data.frame")
