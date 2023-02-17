48ad2b0d9931be512ee45f9abe6e6313d292985c
saiemgilani <saiem.gilani@gmail.com>
2023-02-16 20:58:04 -0500

    update tests

--- tests/testthat/test-espn_wbb_rankings.R
+++ tests/testthat/test-espn_wbb_rankings.R
@@ -1,18 +1,46 @@
 
 cols <- c(
-  "id", "name", "short_name", "type", "headline", 
-  "short_headline", "current", "previous", "points", "first_place_votes", 
-  "trend", "date", "last_updated", "record_summary", "team_id", "team_uid", 
-  "team_location", "team_name", "team_nickname", "team_abbreviation", "team_color",
-  "team_logo", "occurrence_number", "occurrence_type", "occurrence_last", "occurrence_value", 
-  "occurrence_display_value", "season_year", "season_start_date", "season_end_date",
-  "season_display_name", "season_type_type", "season_type_name", 
-  "season_type_abbreviation", "first_occurrence_type", "first_occurrence_value"
+  "id",
+  "headline",
+  "short_headline",
+  "current",
+  "previous",
+  "points",
+  "first_place_votes",
+  "trend",
+  "date",
+  "last_updated",
+  "record_summary",
+  "team_id",
+  "team_uid",
+  "team_location",
+  "team_name",
+  "team_nickname",
+  "team_abbreviation",
+  "team_color",
+  "team_logo",
+  "name",
+  "short_name",
+  "type",
+  "occurrence_number",
+  "occurrence_type",
+  "occurrence_last",
+  "occurrence_value",
+  "occurrence_display_value",
+  "season_year",
+  "season_start_date",
+  "season_end_date",
+  "season_display_name",
+  "season_type_type",
+  "season_type_name",
+  "season_type_abbreviation",
+  "first_occurrence_type",
+  "first_occurrence_value"
 )
 
 test_that("ESPN - WBB Rankings", {
   skip_on_cran()
   x <- espn_wbb_rankings()
-  expect_equal(colnames(x), cols)
+  expect_equal(sort(colnames(x)), sort(cols))
   expect_s3_class(x, "data.frame")
 })
