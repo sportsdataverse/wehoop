03491c64db49725d82a1ce9c18c0b628876fa625
saiemgilani <saiem.gilani@gmail.com>
2023-04-03 15:02:03 -0400

    add condition for broken pbp games with <10 rows

--- R/espn_wbb_data.R
+++ R/espn_wbb_data.R
@@ -2462,10 +2462,13 @@ espn_wbb_player_stats <- function(
 #' @importFrom lubridate with_tz ymd_hm
 #' @export
 helper_espn_wbb_pbp <- function(resp){
+  
   game_json <- resp %>%
     jsonlite::fromJSON()
   pbp_source <- game_json[["header"]][["competitions"]][["playByPlaySource"]]
-  if (pbp_source != "none") {
+  plays <- game_json %>%
+    purrr::pluck("plays")
+  if (pbp_source != "none" && nrow(plays) > 10) {
     homeAway1 <- jsonlite::fromJSON(resp)[['header']][['competitions']][['competitors']][[1]][['homeAway']][1]
     
     gameId <- as.integer(game_json[["header"]][["id"]])
